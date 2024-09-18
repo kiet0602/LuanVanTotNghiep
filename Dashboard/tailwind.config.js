ddc6eb52aacb8c2e2bf284fe1f7af6f03390f01b
kiet/0602 <haidangteen3@gmail.com>
2024-09-18 18:00:04 +0700

    dashboard 10%

---
+++ Dashboard/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Thêm các tệp cần quét ở đây
+    "./public/index.html", // Nếu bạn có tệp HTML ở thư mục public
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
